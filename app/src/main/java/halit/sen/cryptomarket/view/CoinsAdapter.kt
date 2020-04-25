f3dcb1244ed74b3474a1d5600e8722b7909d3baa
Halit Sen <sen.halitt@gmail.com>
2020-04-25 13:25:47 +0300

    coin list design

--- app/src/main/java/halit/sen/cryptomarket/view/CoinsAdapter.kt
+++ app/src/main/java/halit/sen/cryptomarket/view/CoinsAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import halit.sen.cryptomarket.R
 import halit.sen.cryptomarket.model.data.Coin
 import kotlinx.android.synthetic.main.coin_list_item.view.*
+import java.text.DecimalFormat
 
 class CoinsAdapter (): RecyclerView.Adapter<CoinsAdapter.CoinsViewHolder>(){
 
@@ -40,12 +41,23 @@ class CoinsAdapter (): RecyclerView.Adapter<CoinsAdapter.CoinsViewHolder>(){
         val coinChangePercentage = view.coin_change_percentage
 
         fun bind(coin: Coin){
+
+            val price  = java.lang.Double.valueOf(coin.quote.usd.price)
+            val formattedPrice = DecimalFormat("0.000").format(price)
+
+            val floatIntex = coin.quote.usd.percentChangePerDay.indexOf('.')
+            val formattedDailyChange = coin.quote.usd.percentChangePerDay
+                .substring(0, floatIntex + 3)
+
+            val timeIndex =coin.lastUpdated.indexOf('T')
+            val formattedTime = coin.lastUpdated.substring(timeIndex+1,timeIndex +9)
+
             coinName.text = coin.name
             coinSymbol.text = coin.symbol
-            coinPrice.text = coin.quote.usd.price
-            lastUpdate.text = coin.lastUpdated
+            coinPrice.text = "$ $formattedPrice"
+            lastUpdate.text = formattedTime
             //todo burası kullanıcı seçimine göre günlük,saatlik,haftalık olarak değişecek (shared preferences ta tutulacak.)
-            coinChangePercentage.text = "${coin.quote.usd.percentChangePerDay} %"
+            coinChangePercentage.text = "$formattedDailyChange %"
             if((coin.quote.usd.percentChangePerDay).toDouble() > 0){
                 coinArrow.setImageResource(R.drawable.green_arrow_icon)
             }else{
